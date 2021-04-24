00f44ba5e8cbc434899621d25350ee04b6354443
Lorenzo Simpson <lorenzosimp@gmail.com>
2021-04-24 14:22:00 -0400

    ssl

--- knexfile.js
+++ knexfile.js
@@ -46,7 +46,10 @@ module.exports = {
 
    production: {
       client: 'pg',
-      connection: process.env.DATABASE_URL,
+      connection: {
+         connectionString: process.env.DATABASE_URL,
+         ssl: { rejectUnauthorized: false}
+      },
       migrations: {
          directory: './database/migrations'
       },
